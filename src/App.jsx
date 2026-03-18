ccea2ff97034b1b0d42f04619cebf42aca54eac3
Vartolomei Richard <richard.vartolomei@student.unitbv.ro>
2026-03-18 12:39:40 +0200

    Git Hub laborator 1+2+3

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Card from "./Card";
+function App() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Card/>
+    </div>
+  );
+}
+export default App;
+
---
+++ src/Card.jsx
@@ -0,0 +1,10 @@
+function Card() {
+  return (
+    <div>
+      <h3>Banca Transilvania</h3>
+      <p>Card de credit</p>
+    </div>
+  );
+}
+
+export default Card;
