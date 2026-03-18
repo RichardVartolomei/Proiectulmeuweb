bb028be4fd45982da22c8e434809254c6d2b828f
Vartolomei Richard <richard.vartolomei@student.unitbv.ro>
2026-03-18 13:13:32 +0200

    EX.4 Laborator 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import Card from './Card';
 
 function App() {
+  // Array de proiecte
+  const projects = [
+    { title: "Proiect 1", description: "Pagina personala" },
+    { title: "Proiect 2", description: "Calculator buget" },
+    { title: "Proiect 3", description: "Dashboard React" },
+    { title: "Proiect 4", description: "Aplicatie ToDo" },
+    { title: "Proiect 5", description: "Site de prezentare" },
+  ];
+
   return (
     <div>
       <h1>Dashboard</h1>
 
-      <Card title="Proiect 1" description="Pagina personala cu HTML si CSS" />
-      <Card title="Proiect 2" description="Pagina interactiva cu JavaScript" />
-      <Card title="Proiect 3" description="Dashboard cu React" />
+      {/* Generare Card-uri dinamic */}
+      {projects.map((item, index) => {
+        return (
+          <Card
+            key={index} // React trebuie sa aiba key pentru fiecare element generat din map
+            title={item.title}
+            description={item.description}
+          />
+        );
+      })}
     </div>
   );
 }
