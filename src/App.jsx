69c3c2cf51f5b3855c5d0ae200e46c7d39d30f47
Vartolomei Richard <richard.vartolomei@student.unitbv.ro>
2026-03-18 13:15:57 +0200

    EX.5 Laborator 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
+import { useState } from 'react';
 import Card from './Card';
 
 function App() {
-  // Array de proiecte
+  const [count, setCount] = useState(0);
+
   const projects = [
     { title: "Proiect 1", description: "Pagina personala" },
     { title: "Proiect 2", description: "Calculator buget" },
     { title: "Proiect 3", description: "Dashboard React" },
-    { title: "Proiect 4", description: "Aplicatie ToDo" },
-    { title: "Proiect 5", description: "Site de prezentare" },
   ];
 
   return (
     <div>
       <h1>Dashboard</h1>
 
-      {/* Generare Card-uri dinamic */}
-      {projects.map((item, index) => {
-        return (
-          <Card
-            key={index} // React trebuie sa aiba key pentru fiecare element generat din map
-            title={item.title}
-            description={item.description}
-          />
-        );
-      })}
+      {/* EX 5 - Contor */}
+      <h2>Contor</h2>
+      <p>Ai apasat de {count} ori</p>
+      <button onClick={() => setCount(count + 1)}>Click</button>
+
+      {/* EX 4 - Card-uri */}
+      {projects.map((item, index) => (
+        <Card
+          key={index}
+          title={item.title}
+          description={item.description}
+        />
+      ))}
     </div>
   );
 }
