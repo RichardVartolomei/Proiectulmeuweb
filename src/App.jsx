9638aa9415b6c0182eb1ee97a172485c0ca2489f
Vartolomei Richard <richard.vartolomei@student.unitbv.ro>
2026-03-21 15:19:28 +0200

    Laborator 5:ex.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Card from './Card';
-
+import QuickNote from './QuickNote';
 function App() {
   const [count, setCount] = useState(0);
 
@@ -8,6 +8,7 @@ function App() {
     { title: "Proiect 1", description: "Pagina personala" },
     { title: "Proiect 2", description: "Calculator buget" },
     { title: "Proiect 3", description: "Dashboard React" },
+    { title: "Proiect 4", description: "Masa de pranz "},
   ];
 
   return (
@@ -15,14 +16,13 @@ function App() {
       <h1>Dashboard</h1>
 
       {/* Contor */}
-      <h2>Contor</h2>
+      <h2>Contor i</h2>
       <p>Ai apasat de {count} ori</p>
 
       {/* Butoane */}
       <button onClick={() => setCount(count + 1)}>+1</button>
       <button onClick={() => setCount(count - 1)}>-1</button>
       <button onClick={() => setCount(0)}>Reset</button>
-
       {/* Card-uri */}
       <h2>Proiecte</h2>
       {projects.map((item, index) => (
@@ -31,8 +31,10 @@ function App() {
           title={item.title}
           description={item.description}
         />
-      ))}
+      ))}   
+      <QuickNote/>
     </div>
+ 
   );
 }
 
