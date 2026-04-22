53982854132052304779c53d33b99427f74e5cf9
Vartolomei Richard <richard.vartolomei@student.unitbv.ro>
2026-04-22 14:50:17 +0300

    Laboratorul 8 ex.3

--- server/index.js
+++ server/index.js
@@ -2,21 +2,45 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/',function(req,res){
-    res.json({message:'Serverul functioneaza!' });
+app.get('/', function(req, res) {
+    res.json({ message: 'Serverul functioneaza!' });
 });
 
-const projects=[
-  { id : 1, title:"Pagina personala", tech:"HTML, CSS", done:true},
-  { id : 2, title:"Calculator Buget", tech:"JS", done:true},
-  { id : 3, title:"Dashboard React", tech:"React", done:false},
-  { id : 4, title:"API Meteo", tech:"React,API", done:false},
-
+const projects = [
+  { id: 1, title: "Pagina personala", tech: "HTML, CSS", done: true },
+  { id: 2, title: "Calculator Buget", tech: "JS", done: true },
+  { id: 3, title: "Dashboard React", tech: "React", done: false },
+  { id: 4, title: "API Meteo", tech: "React,API", done: false },
 ];
-app.get('/api/projects', function(req, res){
-   res.json(projects);  
+
+// toate proiectele
+app.get('/api/projects', function(req, res) {
+    res.json(projects);
 });
 
-app.listen(PORT,function(){
- console.log('Server pornit pe https://localhost ' + PORT);
+app.get('/api/projects/:id', function(req, res) {
+    const id = parseInt(req.params.id);
+    const project = projects.find(p => p.id === id);
+
+    if (!project) {
+        return res.status(404).json({ error: 'Not found' });
+    }
+
+    res.json(project);
+});
+
+app.get('/api/stats', function(req, res) {
+    const total = projects.length;
+    const finalizate = projects.filter(p => p.done === true).length;
+    const inLucru = projects.filter(p => p.done === false).length;
+
+    res.json({
+        total: total,
+        finalizate: finalizate,
+        inLucru: inLucru
+    });
+});
+
+app.listen(PORT, function() {
+    console.log('Server pornit pe http://localhost:' + PORT);
 });
