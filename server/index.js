0594a0b9b7f158e6f01174853e9d674b1acc9f13
Vartolomei Richard <richard.vartolomei@student.unitbv.ro>
2026-04-22 15:19:56 +0300

    Laboratorul 8 ex.6(bonus)

--- server/index.js
+++ server/index.js
@@ -62,6 +62,24 @@ app.delete('/api/projects/:id',function(req, res){
      
      }
 });
+
+app.put('/api/projects/:id',function(req, res){
+     const project = projects.find(p => p.id === id);
+     const id = parseInt(req.params.id);
+   if(!project){
+    return res.status(404).json({error:"Not found"});
+   }
+   if(req.body.title!=undefined){
+    project.title=req.body.title;
+   }
+   if(req.body.tech!=undefined){
+    project.tech=req.body.tech;
+   }
+   if(req.body.done!=undefined){
+    project.done=req.body.done;
+   }
+   res.json(projects);
+});
 app.listen(PORT, function() {
     console.log('Server pornit pe http://localhost:' + PORT);
 });
